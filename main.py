e3ef4caca7eb51ad3377a641769393ceaeb36fb7
Luiz Felipe <115578538+lzFelipee@users.noreply.github.com>
2026-05-06 19:10:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+#1
+'''segmento = ['Emps', 'Empreenda','Pro','ProSmart','Emps+']
+
+def add_segmento(qual):
+    segmento.append(qual)
+
+def tira_segmento():
+    segmento.pop(0)
+
+add_segmento("business")
+tira_segmento()
+
+print(segmento)'''
+
+#2
+
+'''Funcionario = {
+    'nome' : 'Felipe',
+    'Idade' : 22,
+    'Cidade' : 'São Paulo'
+}
+
+Funcionario ['estaEmpregado'] = 'Sim'
+
+print(Funcionario)'''
+
+#3
+'''soma = [10, 20, 30, 40, 50]
+total = 0
+for soma in soma:
+    total += soma
+
+print(total)'''
+#4
+'''nome =  ["ana", "joão", "maria"]
+for c in range(len(nome)):
+    c = 
+'''
+
+#5
+'''idade = [12, 55, 18, 20, 11, 40]
+for idade in idade:
+    if idade > 18:
+        print(idade)'''
+
